5498063f264364bc3563791f4a0e956282572544
Ajse <digittolu25@gmail.com>
2021-04-25 23:46:53 +0100

    add second solutin to the challenge

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+// function repeatStringNumTimes(str, num) {
+//     if (num < 0) return "";
+//     return str.repeat(num);
+//     }
+//     return str;
+//   }
+//   console.log("hello".repeat(3))
+
 function repeatStringNumTimes(str, num) {
-    if (num < 0) return "";
-    return str.repeat(num);
+    let output = "";
+    if (num < 0) {
+        return "";
     }
-    return str;
+    for (var i=0; i < num; i++) {
+            output += str;
+        }
+    return output;
   }
   
-  repeatStringNumTimes("abc", 3);
-  console.log("hello".repeat(3))
+  repeatStringNumTimes("abc", 6);
