f41c5e0a3b0fee73efb8351dbd30ff17144e9f3e
Ajse <digittolu25@gmail.com>
2021-04-25 23:35:55 +0100

    add repeat method as first solution

---
+++ index.js
@@ -0,0 +1,9 @@
+function repeatStringNumTimes(str, num) {
+    if (num < 0) return "";
+    return str.repeat(num);
+    }
+    return str;
+  }
+  
+  repeatStringNumTimes("abc", 3);
+  console.log("hello".repeat(3))
